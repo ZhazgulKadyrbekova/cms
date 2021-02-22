0c022d3bd61feef75f2df475d4d8d53828e16541
zhazgul004 <zhazgul004@gmail.com>
2021-02-23 00:37:58 +0600

    fixed error with constructor & data in ResponseMessage

--- src/main/java/neobis/cms/Dto/ResponseMessage.java
+++ src/main/java/neobis/cms/Dto/ResponseMessage.java
@@ -1,8 +1,13 @@
 package neobis.cms.Dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 @AllArgsConstructor
+
 public class ResponseMessage {
     private String message;
 }
