666b767511adb5a6b7cb446ab5326ed256452119
zhazgul <zhazgul004@gmail.com>
2021-04-17 15:14:21 +0600

    Testing timezone error

---
+++ src/main/java/neobis/cms/Config/JacksonConfiguration.java
@@ -0,0 +1,17 @@
+package neobis.cms.Config;
+
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer init() {
+        return builder -> builder.timeZone(TimeZone.getTimeZone("Asia/Bishkek"));
+    }
+}
+
